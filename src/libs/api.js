f012a8b714274688e54cd96aa01ac077b9c9b5f1
Kamran <latipovkamran1@gmail.com>
2026-03-06 14:02:38 +0500

    adding env

--- src/libs/api.js
+++ src/libs/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-let accesToken ="eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1YzMwZTNkYWE4NGVjODlkODJlZDlmYWY5MWNmZjMwMiIsIm5iZiI6MTc3MjQ0MTQzNS4wMDQ5OTk5LCJzdWIiOiI2OWE1NGY1YWRlYzRhNzc5YWVkZTkzNDYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.l3JjuKMp6G2EeCdyHvc3CXoOAaZ87iB8u_84Yloy-Ak"
+let accesToken ="eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MDExNmY3NmEwZWJjNTQ3NzE5ZTg1NmQ4NmQ5MmY5ZSIsIm5iZiI6MTcxNDgyMzc0MC41MTUsInN1YiI6IjY2MzYyMjNjNDcwZWFkMDEyMjEyM2UxZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._dZgv2Hqi1IYYtLagRsb_8ELuqQz2_-Ov9-4VnqTTH8"
 
 export const api = axios.create({
     baseURL:" https://api.themoviedb.org/3",
