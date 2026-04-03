6a6d810238984fdb47a498fa7787e501ec8936d6
yusufzhonov <yusufzhonovdave@gmail.com>
2026-04-03 22:40:45 +0500

    netlify test

--- src/libs/api.js
+++ src/libs/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 let accesToken = import.meta.env.VITE_ACCES_TOKEN
 
 export const api = axios.create({
-    baseURL:" https://api.themoviedb.org/3",
+    baseURL:"https://api.themoviedb.org/3",
     headers:{
         Authorization: `Bearer ${accesToken}`
     }
